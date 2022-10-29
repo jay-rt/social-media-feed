4671fc2e47722bc5f50b78d1f6c82d5e4942c315
jay-rt <thapa.jayrt@gmail.com>
2022-10-29 10:23:47 -0400

    added timestamp and emojis to the post

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,8 +1,29 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
+
+const initialEmoji = {
+  thumbsUp: 0,
+  hooray: 0,
+  heart: 0,
+  rocket: 0,
+  eyes: 0,
+};
 
 const initialState = [
-  { id: "1", title: "First Post!", content: "Hello!" },
-  { id: "2", title: "Second Post", content: "More text" },
+  {
+    id: "1",
+    title: "First Post!",
+    content: "Hello!",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: initialEmoji,
+  },
+  {
+    id: "2",
+    title: "Second Post",
+    content: "More text",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: initialEmoji,
+  },
 ];
 
 const postSlice = createSlice({
@@ -20,13 +41,22 @@ const postSlice = createSlice({
             title,
             content,
             userId,
+            date: new Date().toISOString(),
+            reactions: initialEmoji,
           },
         };
       },
+    },
+    reactionAdded: (state, action) => {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
     },
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postSlice.actions;
+export const { postAdded, reactionAdded } = postSlice.actions;
 export default postSlice.reducer;
